ca41805a1cbee608e6680f13fd341874231a1b9b
Chris Flores <floresc9@outlook.com>
2023-08-02 00:49:43 -0400

    Working code
    code is working now.

--- stockticker.js
+++ stockticker.js
@@ -1,43 +1,28 @@
+const fetchData = () => {
+    const apiKey = "Adj2Hk_m_1i3ZpI3LvoI3tTXNWj5Lt33";
+    const ticker = document.getElementById("tickerInput").value;
+    const endPoint = `https://api.polygon.io/v2/aggs/ticker/${ticker}/prev?adjusted=true&apiKey=${apiKey}`;
 
-
-
-//const fetchData = () => {
-
-
-window.onload = (event) => {
-    
-    document.querySelector('form').addEventListener('submit', (event) => {
-    const userInput = document.querySelector('input[type="text"]').value;
-    event.preventDefault();
-
-
-    const endPoint = `https://api.polygon.io/v2/aggs/ticker/${userInput}/prev?adjusted=true&apiKey=Adj2Hk_m_1i3ZpI3LvoI3tTXNWj5Lt33`;
+    // Below is the link teseter, uncomment to show URL.
+    //document.getElementById("displayEndPoint").innerHTML = endPoint;
 
     fetch(endPoint)
-        .then(response => {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            const ticker = data.results[0].T
-            console.log(ticker);
-            const price = data.results[0].c;
-            console.log(price)
-            
+      .then(response => {
+          if (!response.ok) {
+              throw Error(response.statusText);
+          }
+          return response.json();
+      })
+      .then(data => {
+          const ticker = data.results[0].T
+          const price = data.results[0].c;
 
-            let tickerValue = document.querySelector('#ticker');
-            tickerValue.innerHTML = ticker;
-            let priceValue = document.querySelector('#price');
-            priceValue.innerHTML = price;
-
-
-        })
-        .catch(error => {
-            console.log(error);
-        });
-});
-;
-}
-window.onload();
+          let tickerValue = document.querySelector('#ticker');
+          tickerValue.innerHTML = ticker;
+          let priceValue = document.querySelector('#price');
+          priceValue.innerHTML = price;
+      })
+      .catch(error => {
+          console.log(error);
+      });
+  };
